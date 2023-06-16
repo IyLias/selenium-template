4d618eb90b4ed5fc7a1797e7c64c57c923d7cec2
IyLias <iliaskyrin9707@gmail.com>
2023-06-17 01:41:42 +0900

    made selenium template for console and winform

---
+++ ConsoleSeleniumTemplate/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSeleniumTemplate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Crawler crawler = new Crawler();
+            crawler.doWork();
+        }
+    }
+}
---
+++ WinformSeleniumTemplate/Crawler.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinformSeleniumTemplate
+{
+    internal class Crawler
+    {
+        ChromeDriverService chromeDriverService;
+        ChromeDriver chromeDriver;
+
+        public Crawler()
+        {
+            initDriver();
+
+        }
+
+        private void initDriver()
+        {
+            ChromeOptions options = new ChromeOptions();
+            //options.AddArgument("--headless");
+            options.AddArgument("--disable-gpu");
+
+            chromeDriverService = ChromeDriverService.CreateDefaultService();
+
+            // hide chromedriver.exe 
+            //chromeDriverService.HideCommandPromptWindow = true;
+
+            chromeDriver = new ChromeDriver(chromeDriverService, options);
+            // without line below, I got no such element ~~ error. So I need it! 
+            chromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
+        }
+
+
+        public void doWork()
+        {
+            chromeDriver.Navigate().GoToUrl("https://klas.kw.ac.kr");
+        }
+
+
+
+    }
+}
